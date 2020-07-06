3a982e0be74ab4d2475082f4a5edfc257f5a35ed
harshvardhan Mamgai <harshitmamgai11@gmail.com>
2020-07-06 13:58:19 +0530

    web build added

---
+++ web-build/precache-manifest.543d3020b65b0084c3c5684049bd3211.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0636a349418076a1204",
+    "url": "/static/js/app.675d98c8.chunk.js"
+  },
+  {
+    "revision": "77cafc906f42b4a876d5",
+    "url": "/static/js/runtime~app.0dd01ab9.js"
+  },
+  {
+    "revision": "828211f2cfb2ec3ced23",
+    "url": "/static/js/2.11492583.chunk.js"
+  },
+  {
+    "revision": "864b5a2a12e395a0aba167209a78df95",
+    "url": "/static/media/rock.864b5a2a.png"
+  },
+  {
+    "revision": "cffb3100c0baea623b0353f9e933ef62",
+    "url": "/static/media/paper.cffb3100.png"
+  },
+  {
+    "revision": "48d238a67d2b41996cd45194a93b7b10",
+    "url": "/static/media/scissors.48d238a6.png"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "84f65cc123fea0f09d64cc21e361afc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0041c20bf54ed365d47eee55aa441acc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
